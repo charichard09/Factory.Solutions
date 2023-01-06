7f5700fad6d993ab6071728562a3f0ab6342e3d8
Richard <charichard09@gmail.com>
2023-01-06 13:36:46 -0800

    WIP Error handling: add Machines controller and view

---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Factory.Controllers;
+
+public class MachinesController : Controller
+{
+  private readonly FactoryContext _db;
+
+  public MachinesController(FactoryContext db)
+  {
+    _db = db;
+  }
+  
+  public ActionResult Index()
+  {
+    return View(_db.Machines.ToList());
+  }
+
+  public ActionResult Create()
+  {
+    return View();
+  }
+
+  [HttpPost]
+  public ActionResult Create(Machine machine)
+  {
+    _db.Machines.Add(machine);
+    _db.SaveChanges();
+    return RedirectToAction("Index");
+  }
+
+  public ActionResult Details(int id)
+  {
+    Machine thisMachine = _db.Machines
+      .Include(machine => machine.JoinEntities)
+      .ThenInclude(join => join.Machine)
+      .FirstOrDefault(machine => machine.MachineId == id);
+    return View(thisMachine);
+  }
+}
