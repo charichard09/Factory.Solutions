d7be663ec829a089d58db00f19958468daa7ffe4
Richard <charichard09@gmail.com>
2023-01-06 16:14:54 -0800

    add validation checking for creating a machine and an engineer obj

--- Factory/Models/Engineers.cs
+++ Factory/Models/Engineers.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models;
 
 public class Engineer
 {
   public int EngineerId { get; set; }
+  [Required(ErrorMessage = "Please enter a name for this employee.")]
   public string EngineerName { get; set; }
   public List<EngineerMachine> JoinEntities { get; set; }
 }
