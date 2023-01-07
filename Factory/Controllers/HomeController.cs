b7e190e0869af0b268a1c331147d4384ca2eaf95
Richard <charichard09@gmail.com>
2023-01-06 17:04:06 -0800

    add home display of all machines and engineers

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -17,13 +17,12 @@ public class HomeController : Controller
       [HttpGet("/")]
       public ActionResult Index()
       {
-        // Category[] cats = _db.Categories.ToArray();
-        // Item[] items = _db.Items.ToArray();
-        // Dictionary<string, object[]> model = new Dictionary<string, object[]>();
-        // model.Add("categories", cats);
-        // model.Add("items", items); 
-        // return View(model);
-        return View();
+        Engineer[] engineers = _db.Engineers.ToArray();
+        Machine[] machines = _db.Machines.ToArray();
+        Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+        model.Add("engineers", engineers);
+        model.Add("machines", machines); 
+        return View(model);
       }
     }
 }
